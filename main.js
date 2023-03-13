5f5b94f663055658ccf37ac15c1664f5caf46364
JuanCarlosTB <76971659+JUANCARLOST-LAB@users.noreply.github.com>
2023-03-12 18:22:22 -0600

    Practica 5

---
+++ main.js
@@ -0,0 +1,16 @@
+let tipo = document.getElementById("gasto")
+let monto = document.getElementById("monto")
+let campoM = document.getElementById("mostrarArray");
+let presupuestos = []
+function agregar(){
+    let tipo_valor = tipo.value;
+    let monto_valor = monto.value;
+    if(tipo_valor=="" || monto_valor==""){
+        alert("Contiene campos vacios");
+        return;
+    }
+    let informacion = tipo_valor + ": $"+monto_valor;
+    presupuestos.push(informacion)
+    console.log(presupuestos)
+    campoM.innerHTML = presupuestos;
+}
